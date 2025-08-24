8a7efac8f2f0dd0f9231c02370f0b14c2829cea1
reynardsaputra <ge35qum@mytum.de>
2025-08-24 19:48:14 +0200

    edited sports category list

--- components/SportCard.jsx
+++ components/SportCard.jsx
@@ -3,11 +3,14 @@ import { RefreshCcw } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
-const SportCard = ({ title, imageSrc, alt }) => {
+const SportCard = ({ title, imageSrc, alt, backTitle, backContent }) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
   return (
-    <div className="card-container" onClick={() => setIsFlipped(!isFlipped)}>
+    <div
+      className={`card-container ${isFlipped ? "flipped" : ""}`}
+      onClick={() => setIsFlipped(!isFlipped)}
+    >
       <div
         className={`card ${
           isFlipped ? "flipped" : ""
@@ -21,11 +24,24 @@ const SportCard = ({ title, imageSrc, alt }) => {
           <RefreshCcw className="text-white absolute bottom-3" />
         </div>
         <div className="card-back p-6">
-          <h2>Back Side</h2>
-          <p>Content goes here</p>
+          <h2>{backTitle}</h2>
+          <p>{backContent}</p>
           <RefreshCcw className="text-white absolute bottom-3" />
         </div>
       </div>
+      {/* <div className="flex flex-1 justify-center items-center flex-col gap-3 p-6">
+        <h3 className="font-satoshi font-semibold text-white text-2xl mb-10">
+          {title}
+        </h3>
+        <Image src={imageSrc} alt={alt} width={300} height={300} />
+        <RefreshCcw className="text-white absolute bottom-3" />
+      </div>
+      <div className="flex flex-1 justify-center items-center flex-col gap-3 p-6">
+        <h3 className="font-satoshi font-semibold text-white text-2xl mb-10">
+          Back Side
+        </h3>
+        <RefreshCcw className="text-white absolute bottom-3" />
+      </div> */}
     </div>
   );
 };
--- components/SportCardList.jsx
+++ components/SportCardList.jsx
@@ -1,23 +1,12 @@
 import SportCard from "./SportCard";
+import { sportsCardContent } from "@constants";
 
 const SportCardList = () => {
   return (
-    <div className="space-y-6 py-6 xs:columns-1 sm:gap-9 md:columns-3">
-      <SportCard
-        title="Futsal"
-        imageSrc="/images/futsal.png"
-        alt="futsal-photo"
-      />
-      <SportCard
-        title="Basket"
-        imageSrc="/images/basket.png"
-        alt="basket-photo"
-      />
-      <SportCard
-        title="Voli"
-        imageSrc="/images/volley.png"
-        alt="volley-photo"
-      />
+    <div className="space-y-6 py-6 xs:columns-1 sm:gap-9 xl:columns-3">
+      {sportsCardContent.map((sport, index) => (
+        <SportCard key={index} {...sport} />
+      ))}
     </div>
   );
 };
